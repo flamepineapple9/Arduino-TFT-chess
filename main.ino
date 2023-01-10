954185d0722c8ee75d58e896b5457af23b590b88
peble <34178371+flamepineapple9@users.noreply.github.com>
2023-01-10 10:08:25 -0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -214,31 +214,31 @@ void BoardSetup(){
   
   //black pieces
   DrawPiece(0,0,1,1);
-  DrawPiece(0,1,1,2);
-  DrawPiece(0,2,1,3);
-  DrawPiece(0,3,1,4);
-  DrawPiece(0,4,1,5);
-  DrawPiece(0,5,1,3);
-  DrawPiece(0,6,1,2);
-  DrawPiece(0,7,1,1);
+  DrawPiece(1,0,1,2);
+  DrawPiece(2,0,1,3);
+  DrawPiece(3,0,1,4);
+  DrawPiece(4,0,1,5);
+  DrawPiece(5,0,1,3);
+  DrawPiece(6,0,1,2);
+  DrawPiece(7,0,1,1);
   for(int pawns = 0; pawns < 8; pawns++){
-    DrawPiece(1,pawns,1,0);
+    DrawPiece(pawns,1,1,0);
   }
   
   //white pieces
-  DrawPiece(7,0,0,1);
-  DrawPiece(7,1,0,2);
-  DrawPiece(7,2,0,3);
-  DrawPiece(7,3,0,4);
-  DrawPiece(7,4,0,5);
-  DrawPiece(7,5,0,3);
-  DrawPiece(7,6,0,2);
+  DrawPiece(0,7,0,1);
+  DrawPiece(1,7,0,2);
+  DrawPiece(2,7,0,3);
+  DrawPiece(3,7,0,4);
+  DrawPiece(4,7,0,5);
+  DrawPiece(5,7,0,3);
+  DrawPiece(6,7,0,2);
   DrawPiece(7,7,0,1);
   for(int pawns = 0; pawns < 8; pawns++){
-    DrawPiece(6,pawns,0,0);
+    DrawPiece(pawns,6,0,0);
   }
 }
-
+  
 void loop(){
   BoardSetup();
   delay(100000); //this is just here so it doesn't keep drawing everything all the time
