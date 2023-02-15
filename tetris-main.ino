66354a7c26359f583fa445953fade5eaa358f2ba
RyanYasuda <122055573+RyanYasuda@users.noreply.github.com>
2023-02-15 13:47:47 -0800

    For Later

--- tetris-main.ino
+++ tetris-main.ino
@@ -1,1 +1,38 @@
+#include <SPI.h> 
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7735.h>
+#include <ButtonDebounce.h>
 
+// For the joystick
+#define Y_PIN A2 //orange wire
+#define X_PIN A3 //yellow wire
+#define SW_PIN 2 //green wire
+#define MIN_CLICK 100 //debounce ms
+#define MAX_DCLICK 500 //double-click cutoff
+
+//for the lcd rgb display
+#define TFT_SS 7
+#define RST 8
+#define DC 9
+#define SD_SS 10
+
+Adafruit_ST7735 mytft = Adafruit_ST7735(TFT_SS, DC, RST); // idk why this is needed      Grace i'm pretty sure this creates the screen object -Eli
+ButtonDebounce button(SW_PIN, MIN_CLICK);// seems to be defining some values for the button debounce library
+
+//array for colors, pretty sure we can delete this, unless it's needed for the screen to operate somehow. no harm in leaving it though
+unsigned int dispColors[] = { ST7735_BLACK, ST7735_RED, ST7735_ORANGE, ST7735_YELLOW, ST7735_GREEN, ST7735_CYAN, ST7735_BLUE, ST7735_MAGENTA, ST7735_WHITE };
+byte numColors =  sizeof(dispColors) / sizeof(dispColors[0]); //how many colors
+
+void setup() {
+  mytft.initR(INITR_BLACKTAB); // for 1.8' TFT
+  mytft.setTextWrap(false); // Allow text to run off the edge
+  mytft.setRotation(0);
+  mytft.fillScreen(ST7735_BLACK); //sets bg as black
+  mytft.setTextSize(1); //smallest text size
+  mytft.setTextColor(ST7735_WHITE);
+  mytft.setCursor(0,0); //puts cursor in top left, (which is where the text is printed from)
+  randomSeed(analogRead(A5)); //Seed with bogus read
+  Serial.begin(9600);
+  
+  //game setup
+  BoardSetup(true);
